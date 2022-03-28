929f45bf248e005be18080de110901cdabf977d9
Aman7Sharma <aironmanmk85@gmail.com>
2022-03-28 20:14:55 +0530

    Task

---
+++ Task/src/App.js
@@ -0,0 +1,215 @@
+import './App.css';
+
+function App() {
+  const current =new Date();
+  const date=`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+ 
+  return (
+    <div className="App">
+      <div className="sidebar" >
+        <br></br>
+      <div className="filter"></div>
+        <div className="search">
+          <input type="search" placeholder="Search"></input>
+        </div>
+        
+        <div align="left" className="chat">
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+          <div className="chats">
+            <h4>Tentative Text</h4>
+            <div className="date" >
+              <p>{date}</p>
+            </div>           
+            <p>Stripe Test</p>
+            <input type={'date'}></input>
+          </div>
+
+        </div>
+
+
+        <div className="book">
+          <button className="booking" >CREATE BOOKING</button>
+        </div>
+
+      </div>
+      <div align="center" className="main">
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+        <div className="calender">.  .<br></br><div className="mouth"></div></div> <br></br><br></br>
+        <h1>Reservations</h1><br></br>
+        <p>Select any reservation item </p><br></br>
+        <button className="Reser" >Create Reservation</button><br></br>
+        <button className="Reser" >Create Booking with Quote</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
